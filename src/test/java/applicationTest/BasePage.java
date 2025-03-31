26ee207da3dd083f08092510a8093fbb84fc1ccc
Jefferson Soares <jeffersonsoares.melo@gmail.com>
2025-03-31 08:45:30 -0300

    Novo Projeto de automação de testes com funcionalidade de registro de usuário

---
+++ src/test/java/applicationTest/BasePage.java
@@ -0,0 +1,12 @@
+package applicationTest;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    protected WebDriver navegador;
+
+    public BasePage(WebDriver navegador) {
+
+    }
+}
---
+++ src/test/java/applicationTest/teste/UsuarioRegistroTest.java
@@ -0,0 +1,33 @@
+package applicationTest.teste;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Testes para Registrar um novo usuário")
+public class UsuarioRegistroTest {
+
+    @Test
+    public void testeRegistroUsuarioComSucesso() {
+
+        //Instanciando o ChromeDriver e abrindo o navegador maximizado
+        WebDriver navegador = new ChromeDriver(new ChromeOptions().addArguments("--start-maximized"));
+
+        //Navegando até a página de registro do site proposto
+        navegador.get("https://demo.automationtesting.in/Register.html");
+
+        navegador.getTitle();
+
+        assertEquals("Register", navegador.getTitle(), "O título da página não corresponde ao esperado");
+
+
+
+
+        navegador.close();
+    }
+}
